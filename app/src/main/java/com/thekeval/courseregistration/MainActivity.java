e4296be5186741ae699e51d5b5874faa4a6c10fd
Keval <keval.langalia@live.com>
2021-04-16 19:46:57 +0530

    MainActivity UI done

--- app/src/main/java/com/thekeval/courseregistration/MainActivity.java
+++ app/src/main/java/com/thekeval/courseregistration/MainActivity.java
@@ -3,8 +3,11 @@ package com.thekeval.courseregistration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvWelcomeMsg, tvCourseFees, tvCourseHours, tvTotalFees, tvTotalHours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
